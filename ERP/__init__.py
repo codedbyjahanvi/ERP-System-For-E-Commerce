0621e3cd7d15d45d26aad4ac3b2ec85d59dfb809
DhruviKatariya <dhruvikatariya2004@gmail.com>
2026-01-12 13:07:29 +0530

    Updated feature

--- ERP/__init__.py
+++ ERP/__init__.py
@@ -7,33 +7,33 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] =b'_5#y2L"F4Q8z\n\xec]/'
-    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.config['SECRET_KEY'] ='secret-key'
+    #app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqllite"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:dhruvi%402004@localhost:3306/flask_auth"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     login_manager = LoginManager(app)
-    login_manager.login_view= 'pages.login'
+    login_manager.login_view= 'account.login'
     db.init_app(app)
     
 
-    from .models import User
+    from .models import users
     with app.app_context():
         db.create_all()
 
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id)) 
+        return users.query.get(int(user_id)) 
     
-    from .dashboard import dashboard
+    from .dashboards import dashboards
     from .apps import apps
     from .layouts import layouts    
     from .account import account
     from .components import components
 
-    app.register_blueprint(dashboard ,url_prefix="/")
+    app.register_blueprint(dashboards ,url_prefix="/")
     app.register_blueprint(apps ,url_prefix="/")
     app.register_blueprint(layouts ,url_prefix="/")
     app.register_blueprint(account ,url_prefix="/")
--- ERP/account.py
+++ ERP/account.py
@@ -1,63 +1,63 @@
 from flask import Blueprint,render_template,request,redirect,url_for,flash
 from flask_login import login_user,logout_user,login_required
 from pyrfc3339 import generate
-from .models import User
+from .models import users
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 
-pages = Blueprint('account',__name__,template_folder='templates',
+account = Blueprint('account',__name__,template_folder='templates',
     static_folder='static',)
     
 #Actual Auth pages(working)  
 #Actual Auth pages(working)  
-@pages.route('/login')  
+@account.route('/login')  
 def login():
     return render_template('account/login.html')
 
-@pages.route('/login',methods=['POST'])  
+@account.route('/login',methods=['POST'])  
 def login_post():
     if request.method == 'POST':
         email = request.form.get('email') 
         password = request.form.get('password')
         remember = True if request.form.get('remember') else False
 
-        user = User.query.filter_by(email=email).first()
+        user = users.query.filter_by(email=email).first()
 
         if not user or not check_password_hash(user.password,password):
             flash("Invalid Credentials")
-            return redirect(url_for('pages.login'))
+            return redirect(url_for('account.login'))
 
         login_user(user, remember=remember)
         return redirect(url_for('dashboards.index'))
 
-@pages.route('/signup')  
+@account.route('/signup')  
 def signup(): 
     return render_template('account/signup.html')
 
-@pages.route('/signup',methods=['POST'])  
+@account.route('/signup',methods=['POST'])  
 def signup_post():
     email = request.form.get('email') 
     username = request.form.get('username')
     password = request.form.get('password')
 
-    user_email = User.query.filter_by(email=email).first()
-    user_username = User.query.filter_by(username=username).first()    
+    user_email = users.query.filter_by(email=email).first()
+    user_username = users.query.filter_by(username=username).first()    
     
     if user_email:
         flash("User email already Exists")
-        return redirect(url_for('pages.signup'))
+        return redirect(url_for('account.signup'))
     if user_username:    
         flash("Username already Exists")
-        return redirect(url_for('pages.signup'))
+        return redirect(url_for('account.signup'))
 
-    new_user = User(email=email,username=username,password=generate_password_hash(password,method="sha256"))
+    new_user = users(email=email,username=username,password=generate_password_hash(password,method="sha256"))
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('pages.login'))
+    return redirect(url_for('account.login'))
 
-@pages.route('/logout')
+@account.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('pages.login'))
+    return redirect(url_for('account.login'))
